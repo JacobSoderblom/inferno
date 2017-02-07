36746ea97f2b280e068d04efd3c111d576b5a8f1
Jacob Söderblom <jacob.soderblom@outlook.com>
2017-02-07 11:20:32 +0100

    Query fields helper to get observables

---
+++ packages/inferno-apollo/src/utils/observableQueryFields.ts
@@ -0,0 +1,14 @@
+import { pick } from 'lodash';
+
+export default function observableQueryFields(observable) {
+	const fields = pick(observable, 'variables',
+		'refetch', 'fetchMore', 'updateQuery', 'startPolling', 'stopPolling', 'subscribeToMore');
+
+	Object.keys(fields).forEach((key) => {
+		if (typeof fields[key] === 'function') {
+			fields[key] = fields[key].bind(observable);
+		}
+	});
+
+	return fields;
+}
