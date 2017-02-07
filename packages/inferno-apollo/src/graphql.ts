74218624ab74cb79c82b8e29eddf26cf82145e37
Jacob Söderblom <jacob.soderblom@outlook.com>
2017-02-07 11:22:03 +0100

    GraphQL function to wrap component with apollo

--- packages/inferno-apollo/src/graphql.ts
+++ packages/inferno-apollo/src/graphql.ts
@@ -1,57 +1,169 @@
+import { DocumentNode } from 'graphql';
+
 import Component from 'inferno-component';
 import createElement from 'inferno-create-element';
 import hoistNonInfernoStatic from 'hoist-non-inferno-statics';
-import {
-	DocumentNode
-} from 'graphql';
+import { throwError } from 'inferno-helpers';
 
-import { OperationOption, QueryOptions, MutationOptions } from './types';
-// import parser from './utils/parser';
+import ApolloHelper from './apolloHelper';
+import { OperationOption, QueryOptions, DocumentType } from './types';
+import parser from './utils/parser';
 import getDisplayName from './utils/getDisplayName';
+import shallowEqual from './utils/shallowEqual';
 
 // Helps track hot reloading
-// let nextVersion: number = 0;
+let nextVersion: number = 0;
 
 export default function graphql(
 	document: DocumentNode,
 	operationOptions: OperationOption = {}
 ) {
 	// extract options
-	const {
-		options,
-		skip,
-		// props,
-		alias = 'Apollo'
-	} = operationOptions;
-
-	let mapPropsToOptions = options as (props: any) => QueryOptions | MutationOptions;
-
-	if (typeof mapPropsToOptions !== 'function') {
-		mapPropsToOptions = () => options;
-	}
-
-	let mapPropsToSkip = skip as (props: any) => boolean;
-
-	if (typeof mapPropsToSkip !== 'function') {
-		mapPropsToSkip = (() => skip as any);
-	}
-
-	// const mapResultToProps = operationOptions.props;
+	const alias = 'Apollo';
 
 	// safety check on the operation
-	// const operation = parser(document);
+	const operation = parser(document);
 
 	// Helps track hot reloading.
-	// const version = nextVersion++;
+	const version = nextVersion++;
 
 	const wrapWithApolloClient = (WrappedComponent: any) => {
 		const graphQLDisplayName = `${alias}(${getDisplayName(WrappedComponent)})`;
+		const wrappedComponentDisplayName = getDisplayName(WrappedComponent);
 
 		class GraphQL extends Component<any, any> {
 			static displayName = graphQLDisplayName;
 			static WrappedComponent = WrappedComponent;
 
+			// Needed by HMR
+			public props: any;
+			public version: number;
+			public hasMounted: boolean;
+
+			private apolloHelper: ApolloHelper;
+			private shouldRender: Boolean;
+			private shouldRerender: Boolean;
+			private renderedElement: any;
+
+			constructor(props, context) {
+				super(props, context);
+				this.version = version;
+				if (!context.client) {
+					throwError(
+						`Could not find "client" in the context of ` +
+						`"${graphQLDisplayName}". ` +
+						`Wrap the root component in an <ApolloProvider>`
+					);
+				}
+				this.apolloHelper = new ApolloHelper({
+					client: context.client,
+					type: operation.type,
+					document,
+					displayName: graphQLDisplayName,
+					operationOptions,
+					operation,
+					wrappedComponentDisplayName,
+					forceRenderChildren: this.forceRenderChildren
+				});
+
+				if (!this.shouldSkip(props)) {
+					this.setInitialProps();
+				}
+			}
+
+			onComponentDidMount() {
+				this.hasMounted = true;
+				if (!this.apolloHelper.isType(DocumentType.Mutation)
+					&& !this.shouldSkip(this.props)) {
+					this.apolloHelper.subscribeToQuery();
+				}
+			}
+
+			onComponentWillUpdate(lastProps, nextProps) {
+				if (shallowEqual(lastProps, nextProps)) {
+					return;
+				}
+
+				this.shouldRender = true;
+
+				if (this.apolloHelper.isType(DocumentType.Mutation)) {
+					return;
+				}
+
+				if (this.apolloHelper.isType(DocumentType.Subscription)
+					&& operationOptions.shouldResubscribe
+					&& operationOptions.shouldResubscribe(lastProps, nextProps)) {
+						this.apolloHelper.unsubscribeFromQuery();
+						this.apolloHelper.deleteQueryObservable();
+						this.apolloHelper.updateQuery(nextProps);
+						this.apolloHelper.subscribeToQuery();
+						return;
+				}
+
+				if (this.shouldSkip(nextProps)) {
+					if (!this.shouldSkip(lastProps)) {
+						this.apolloHelper.unsubscribeFromQuery();
+					}
+
+					return;
+				}
+
+				this.apolloHelper.updateQuery(nextProps);
+				this.apolloHelper.subscribeToQuery();
+			}
+
+			onComponentWillUnmount() {
+				if (this.apolloHelper.isType(DocumentType.Mutation)
+					|| this.apolloHelper.isType(DocumentType.Subscription)) {
+					this.apolloHelper.unsubscribeFromQuery();
+				}
+
+				this.hasMounted = false;
+			}
+
+			shouldSkip(props: any = this.props) {
+				return this.apolloHelper.mapPropsToSkip(props) ||
+					(this.apolloHelper.mapPropsToOptions(props) as QueryOptions).skip;
+			}
+
+			setInitialProps() {
+				if (this.apolloHelper.isType(DocumentType.Mutation)) {
+					return;
+				}
+
+				const opts = this.apolloHelper.calculateOptions(this.props);
+				this.apolloHelper.createQuery(opts);
+			}
+
+			forceRenderChildren = () => {
+				this.shouldRerender = true;
+				if (this.hasMounted) {
+					this.setState({});
+				}
+			}
+
 			render() {
+				if (this.shouldSkip()) {
+					return createElement(WrappedComponent, this.props);
+				}
+
+				const { shouldRerender, renderedElement, props } = this;
+				this.shouldRerender = false;
+
+				const data = this.apolloHelper.getDataForChild(props);
+				const clientProps = this.apolloHelper.calculateResultProps(data, props);
+				const mergedPropsAndData = Object.assign({}, props, clientProps);
+
+				if (!shouldRerender && renderedElement) {
+					return renderedElement;
+				}
+
+				if (operationOptions.withRef) {
+					mergedPropsAndData.ref = 'wrappedInstance';
+				}
+
+				this.renderedElement = createElement(WrappedComponent, mergedPropsAndData);
+
 				return createElement(WrappedComponent, Object.assign({}, this.props));
 			}
 		}
---
+++ packages/inferno-apollo/src/index.ts
@@ -0,0 +1,7 @@
+export { default as ApolloProvider } from './ApolloProvider';
+export { default as graphql } from './graphql';
+export { default as withApollo } from './withApollo';
+export { InjectedGraphQLProps } from './types';
+
+// expose easy way to join queries from redux
+export { compose } from 'redux';
